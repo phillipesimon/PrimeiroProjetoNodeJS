4b1a6a5e75ab23c4fd546261690f75407ed1baad
phillipesimon <phillipe.simon@gmail.com>
2023-11-29 16:48:31 -0300

    Consumindo uma stream completa

--- streams/fake-upload-to-http-stream.js
+++ streams/fake-upload-to-http-stream.js
@@ -7,7 +7,7 @@ class OneToHundreadStream extends Readable {
     const i = this.index++
 
     setTimeout(() => {
-      if (i > 100) {
+      if (i > 10) {
         this.push(null)
       } else {
         const buf = Buffer.from(String(i))
@@ -22,3 +22,12 @@ fetch('http://localhost:3334', {
   method: 'POST',
   body: new OneToHundreadStream(),
 })
+  .then((response) => {
+    return response.text()
+  })
+  .then((data) => {
+    console.log(data)
+  })
+
+//Para testar rode no terminal primeiro o node streams/stream-http-server.js
+//e em outro terminal rode o comando node streams/fake-upload-to-http-stream.js
