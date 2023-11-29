783d057e1c168e95df0202813b3be602b9373d3f
phillipesimon <phillipe.simon@gmail.com>
2023-11-29 17:27:13 -0300

    Entendendo Buffers no Node

---
+++ streams/buffer.js
@@ -0,0 +1,6 @@
+const buf = Buffer.from('ok')
+
+console.log(buf) // retorna o valor salvo na memoria em hexadecimal
+console.log(buf.toJSON()) // retorna o valor salvo em decimal
+
+//node rode streams/buffer.js
