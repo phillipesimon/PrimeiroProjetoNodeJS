14a0e2d3446798de8a17c024ddd7eb8600de776b
phillipesimon <phillipe.simon@gmail.com>
2023-11-30 17:56:14 -0300

    Ajuste - Recuperando os dados do banco de dados

--- src/database.js
+++ src/database.js
@@ -5,6 +5,16 @@ const databasePath = new URL('../db.json', import.meta.url)
 export class Database {
   #database = {}
 
+  constructor() {
+    fs.readFile(databasePath, 'utf8')
+      .then((data) => {
+        this.#database = JSON.parse(data)
+      })
+      .catch(() => {
+        this.#persist()
+      })
+  }
+
   #persist() {
     fs.writeFile(databasePath, JSON.stringify(this.#database))
   }
